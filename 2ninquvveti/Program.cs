2f9176109ed66ecf905f1d1d5b504e70429b58a9
Nurlan <nurlanea@code.edu.az>
2023-11-02 20:44:46 +0400

    upload

---
+++ 2ninquvveti/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(power(32));
+
+string power (int n)
+{   int two = 2;
+    while (n > two)
+    {
+        if (n==1)
+        {
+            return $"{n} 2nin quvvetidir";
+        }
+
+        if (n % two == 0)
+        {
+            two *= 2;
+
+            if (n / two == 1)
+            return $"{n} 2nin quvvetidir";
+        }
+    }
+return $"{n} 2nin quvveti deyil";
+}
